065ebf9a8af02a444aee96ad1aec5ab2b39a145b
Severin Steiner <severinsteiner94@gmail.com>
2020-01-25 21:42:24 +0100

    Translated comments to english

--- main/main.ino
+++ main/main.ino
@@ -5,26 +5,19 @@
 
   Version: 21.01.2020
 
-  Autoren:
-  - Senti Laurin <laurin.senti@edu.tbz.ch>
-  - Severin Steiner <severin.steiner@edu.tbz.ch>
-  - Markus Ritzmann <markus.ritzmann@edu.tbz.ch>
-
-  Im Unterricht von Christoph Jäger.
-
-  Abhängigkeiten
+  Dependencies
   ==============
 
-  Installieren via "Tools -> Manage Libraries"
+  Install in Arduino IDE at "Tools -> Manage Libraries"
 
-  Für den Sensor:
+  For the BME680 sensor:
   - Adafruit BME680 Library by Adafruit
   - Adafruit Unified Sensor by Adafruit
 
-  Für das WLAN:
+  For WiFi:
   - WiFiNNA by Arduino
 
-  Für den TFT Display:
+  For Joy-IT 1.9" TFT Display:
   - TFT Built-in by Arduino, Adafruit
   - Adafruit GFX Library
   - Adafruit ST7735 and ST7789 Library
@@ -35,8 +28,8 @@
 #include <SPI.h>
 
 // TFT: Libraries
-#include <Adafruit_GFX.h>    // Adafruit Grafik-Bibliothek wird benötigt "Adafruit GFX Library"
-#include <Adafruit_ST7735.h> // Adafruit ST7735-Bibliothek wird benötigt "Adafruit ST7735 and ST7789 Library"
+#include <Adafruit_GFX.h>    // requires "Adafruit GFX Library"
+#include <Adafruit_ST7735.h> // requires "Adafruit ST7735 and ST7789 Library"
 
 // Sensor: Libraries
 #include <Adafruit_Sensor.h>
@@ -46,33 +39,33 @@
 #include <WiFiNINA.h>
 #include "arduino_secrets.h"
 
-// TFT: Variablen
-#define TFT_PIN_CS 0         // Arduino-Pin an Display CS
-#define TFT_PIN_DC 6         // Arduino-Pin an
+// TFT: Variables
+#define TFT_PIN_CS 0         // Arduino-Pin on Display CS
+#define TFT_PIN_DC 6         // Arduino-Pin
 #define TFT_PIN_RST 1        // Arduino Reset-Pin
 
-// Sensor: Variablen
+// Sensor: Variables
 #define BME_SCK 9
 #define BME_MISO 10
 #define BME_MOSI 8
 #define BME_CS 7
 
-// Standard Wert aus Dokumentation war 1013.25. Anhand https://kachelmannwetter.com/ch/messwerte/zuerich/luftdruck-qnh/20200119-1500z.html
-// and die Schweiz (Zürich) angepasst. Muss anhand Wetter und Ort angepasst werden.
+// Default value from documentation was 1013.25. According to https://kachelmannwetter.com/ch/messwerte/zuerich/luftdruck-qnh/20200119-1500z.html
+// calibrated to Schweiz (Zürich) Adjust to your location.
 #define SEALEVELPRESSURE_HPA (1036.5)
 
-// TFT: Einstellungen
+// TFT: Settings
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_PIN_CS, TFT_PIN_DC, TFT_PIN_RST);
 
-// Sensor: Einstellugen
+// Sensor: Settings
 Adafruit_BME680 bme(BME_CS); // hardware SPI
 
-//WiFi Einstellungen
+//WiFi: Settings, define these in arduino_secrets.h
 char ssid[] = SECRET_SSID;        // your network SSID (name)
 char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
 int status = WL_IDLE_STATUS;     // the Wifi radio's status
 
-WiFiClient client; //For HTTP requests to send data to influxDB
+WiFiClient client; //For HTTP requests to send data by POST request to influxDB
 char buf[influxdbBufferSize] = {'\0'};
 const String strInfluxDbServer = influxdbServer; //Convert to String
 const String strInfluxDbPort = String(influxdbPort); //Convert to String
@@ -93,10 +86,10 @@ int intGasState;
 void setup() {
 
   // Sets the Digital Pin as output
-  pinMode(2, OUTPUT); // Tongeber
-  pinMode(3, OUTPUT); // LED Rot
-  pinMode(4, OUTPUT); // LED Gelb
-  pinMode(5, OUTPUT); // LED Grün
+  pinMode(2, OUTPUT); // Buzzer
+  pinMode(3, OUTPUT); // LED red
+  pinMode(4, OUTPUT); // LED yellow
+  pinMode(5, OUTPUT); // LED green
 
   // TFT
   tft.initR(INITR_GREENTAB); // Initialise TFT library
@@ -138,26 +131,26 @@ void loop() {
 
 // Status OK
 float statusOk() {
-  digitalWrite(5, HIGH);
+  digitalWrite(5, HIGH); //LED green
 }
 
 // Status Warnung
 float statusWarning() {
-  digitalWrite(4, HIGH);
+  digitalWrite(4, HIGH); //LED yellow
 }
 
 // Status Kritisch
 float statusCritical() {
-  digitalWrite(3, HIGH); // LED
-  digitalWrite(2, HIGH); // Tongeber
+  digitalWrite(3, HIGH); // LED red
+  digitalWrite(2, HIGH); // Buzzer
 }
 
 // Setzt Ausgaben zurück
 float statusReset() {
-  digitalWrite(5, LOW); // LED Grün
-  digitalWrite(4, LOW); // LED Gelb
-  digitalWrite(3, LOW); // LED Rot
-  digitalWrite(2, LOW); // Tongeber
+  digitalWrite(5, LOW); // LED green
+  digitalWrite(4, LOW); // LED yellow
+  digitalWrite(3, LOW); // LED red
+  digitalWrite(2, LOW); // Buzzer
 }
 
 void printWifiData() {
@@ -215,15 +208,15 @@ void sendData(char* data, int influxdbBufferSize) {
     //Print the buffer on the serial line to see how it looks
     Serial.print("Sending following dataset to InfluxDB: ");
     Serial.println(buf);
-    client.println("POST /api/v2/write?org=" + influxdbOrgId + "&bucket=" + influxdbBucketName + "&precision=s HTTP/1.1");
-    client.println("Host: " + strInfluxDbServer + ":" + strInfluxDbPort + "");
+    client.println("POST /api/v2/write?org=" + influxdbOrgId + "&bucket=" + influxdbBucketName + "&precision=s HTTP/1.1"); //start POST request
+    client.println("Host: " + strInfluxDbServer + ":" + strInfluxDbPort + ""); //set various Request Headers
     client.println("Authorization: Token " + influxdbAuthToken);
     client.println("Content-Type: application/x-www-form-urlencoded");
     client.println("Connection: close");
     client.print("Content-Length: ");
     client.println(influxdbBufferSize);
     client.println();
-    client.println(data);
+    client.println(data); //set request content
     client.stop();
   }
   else {
@@ -306,9 +299,9 @@ void setDisplay() {
   String Line10 = String(getAltitude()) + "m";
 
   // Gibt Text aus
-  tft.setCursor(7, 0);            // Setze Position
-  tft.setTextSize(1);             // Schriftgröße einstellen
-  tft.print(Line1);               // Text ausgeben
+  tft.setCursor(7, 0);            // Set position
+  tft.setTextSize(1);             // Set font size
+  tft.print(Line1);               // Output text
 
   tft.setCursor(7, 10);
   tft.setTextSize(2);
